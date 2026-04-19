95a46d75ff5790ebb7344f59a313db16037c1dd3
Oskar Straszynski <oskar.straszynski@gmail.com>
2026-04-19 22:34:21 +0200

    migration

--- src/migrations/20260419_203056_add_image_sizes.ts
+++ src/migrations/20260419_203056_add_image_sizes.ts
@@ -26,24 +26,18 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   ALTER TABLE "media" ADD COLUMN "sizes_case_study_detail_mime_type" varchar;
   ALTER TABLE "media" ADD COLUMN "sizes_case_study_detail_filesize" numeric;
   ALTER TABLE "media" ADD COLUMN "sizes_case_study_detail_filename" varchar;
-  ALTER TABLE "projects" ADD COLUMN "image_id" integer;
-  ALTER TABLE "projects" ADD CONSTRAINT "projects_image_id_media_id_fk" FOREIGN KEY ("image_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
   CREATE INDEX "media_sizes_thumbnail_sizes_thumbnail_filename_idx" ON "media" USING btree ("sizes_thumbnail_filename");
   CREATE INDEX "media_sizes_project_card_sizes_project_card_filename_idx" ON "media" USING btree ("sizes_project_card_filename");
   CREATE INDEX "media_sizes_journal_hero_sizes_journal_hero_filename_idx" ON "media" USING btree ("sizes_journal_hero_filename");
-  CREATE INDEX "media_sizes_case_study_detail_sizes_case_study_detail_fi_idx" ON "media" USING btree ("sizes_case_study_detail_filename");
-  CREATE INDEX "projects_image_idx" ON "projects" USING btree ("image_id");`)
+  CREATE INDEX "media_sizes_case_study_detail_sizes_case_study_detail_fi_idx" ON "media" USING btree ("sizes_case_study_detail_filename");`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "projects" DROP CONSTRAINT "projects_image_id_media_id_fk";
-  
   DROP INDEX "media_sizes_thumbnail_sizes_thumbnail_filename_idx";
   DROP INDEX "media_sizes_project_card_sizes_project_card_filename_idx";
   DROP INDEX "media_sizes_journal_hero_sizes_journal_hero_filename_idx";
   DROP INDEX "media_sizes_case_study_detail_sizes_case_study_detail_fi_idx";
-  DROP INDEX "projects_image_idx";
   ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_url";
   ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_width";
   ALTER TABLE "media" DROP COLUMN "sizes_thumbnail_height";
@@ -67,6 +61,5 @@ export async function down({ db, payload, req }: MigrateDownArgs): Promise<void>
   ALTER TABLE "media" DROP COLUMN "sizes_case_study_detail_height";
   ALTER TABLE "media" DROP COLUMN "sizes_case_study_detail_mime_type";
   ALTER TABLE "media" DROP COLUMN "sizes_case_study_detail_filesize";
-  ALTER TABLE "media" DROP COLUMN "sizes_case_study_detail_filename";
-  ALTER TABLE "projects" DROP COLUMN "image_id";`)
+  ALTER TABLE "media" DROP COLUMN "sizes_case_study_detail_filename";`)
 }
